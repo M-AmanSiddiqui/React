f8a19f389762e44f7ef417e5aac5ccfc6b32bacc
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2024-09-15 21:23:36 +0500

    Second comment

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,23 @@ import Testimonials from './components/Testimonials'
 import Footer from './components/Footer'
 function App() {
   console.log("landingPageData", landingPageData);
-  const {header, hero , features , testimonials} = landingPageData
+  const {header, hero , features , testimonials} = landingPageData;
+  const [userName,setUserName] = useState ('Aman')
+  // const [counter,setcounter] = useState (0)
+  // const handleUpdateCounter = () =>{
+  //   setcounter(counter + 1)
+  // } 
   return (
     <div>
       {/* Header */}
     <Header header={header} />
+        {/* Use with input */}
+
+    {/* Use with number */}
+    {/* <div  className='flex justify-center items-center flex-col'>
+    <h2>{counter}</h2><h2>{userName}</h2>
+    <button onClick={handleUpdateCounter}>Update counter</button>
+    </div> */}
   {/* Hero */}
  <Hero hero={hero} />
 {/* Features Section */}
