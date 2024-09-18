159a40dd6c3ec9f4796c5c28cb8766220a499fbd
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2024-09-19 03:05:04 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -14,69 +14,11 @@ function App() {
   console.log("landingPageData", landingPageData);
   const {header, hero , features , testimonials} = landingPageData;
 
-
-  const [amount , setamount] = useState (0);
-  const [type,setType] = useState ("income");
-  const [transactions,setTransactions] = useState ([]);
-
-  const handleAddTransactions = () =>{
-    setTransactions([...transactions , {amount,type}])
-    
-    
-  };
-  console.log("transactions",transactions);
-    
     return (
     <div>
+
       {/* Header */}
     <Header header={header} />
-    <div className='flex flex-col justify-center items-center'>
-      <div className='flex'>
-<input 
-value={amount}
-onChange={(e) => setamount(e.target.value)}
- className="border border-purple-600 rounded-sm m-4 mx-2 p-1" type="number" placeholder='add Amount'/>
-<select value={type}
-onChange={(e) => setType(e.target.value)}
-className='border border-purple-600 rounded-sm m-4 mx-2 p-1'> 
-  <option value="income">income</option>
-  <option value="expense">expense</option>
-
-</select>
-<button onClick={handleAddTransactions} className='border border-purple-600 rounded-sm m-4 mx-2 p-1'>submit</button>
-      </div>
-      <div>
-        {
-          transactions.map((data , index)=>{
-            return (
-             <div key={index} className='flex g' ><h1 className='font-bold underline text-3xl w-60'>{data.amount}</h1>
-            <h1 className={`font-bold underline text-3xl ${data.type === "income" ? "text-green-700" : "text-red-700"  }`}
-             >
-              {data.type}</h1>
-            </div>
-            );
-          })}
-        
-      </div>
-    </div>
-
-
-
-        {/* Use with input */}
-        {/* <div  className='flex justify-center items-center flex-col'>
-          <input
-           value={userName} 
-          placeholder='Add Your Name' 
-          onChange={(e)=> setUserName(e.target.value)}
-          className='border border purple-400'
-          />
-          </div> */}
-
-    {/* Use with number */}
-    {/* <div  className='flex justify-center items-center flex-col'>
-    <h2>{counter}</h2><h2>{userName}</h2>
-    <button onClick={handleUpdateCounter}>Update counter</button>
-    </div> */}
   {/* Hero */}
  <Hero hero={hero} />
 {/* Features Section */}
